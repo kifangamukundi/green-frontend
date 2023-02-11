1697857aebc0a16b23613cc23e484abf0e8edac3
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2023-02-11 07:46:48 +0300

    Added better listing design

--- src/pages/CenterManagement.jsx
+++ src/pages/CenterManagement.jsx
@@ -1,5 +1,47 @@
 import { Helmet } from 'react-helmet-async';
-import styles from "../style";
+import styles, { layout } from "../style";
+import { star  } from "../assets";
+
+const points = [
+  {
+    id: "feature-1",
+    icon: star,
+    content:
+      "Coordination and planning for production safe storage household’s including institutions food requirements and for various markets.",
+  },
+  {
+    id: "feature-2",
+    icon: star,
+    content:
+      "Provide facilities and space for produce aggregation, loading and unloading ramps, value addition, drying and storage.",
+  },
+  {
+    id: "feature-3",
+    icon: star,
+    content:
+      "Provide a platform for all service providers/ occupations especially youth and women to offer quality services to value chain actors at competitive rates.",
+  },
+  {
+    id: "feature-4",
+    icon: star,
+    content:
+      "Provide training facilities including ITK knowledge exchange platform, nutrition, wellness and recreational center.",
+  },
+];
+
+const ListPoints = ({ icon, content, index }) => (
+  <div className={`flex flex-col p-6 rounded-[20px] w-1/2 ${index !== points.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </div>
+);
+
 
 const CenterManagement = () => (
   <div>
@@ -27,14 +69,15 @@ const CenterManagement = () => (
         <p className={`${styles.paragraph} mt-5`}>
         The team will:
         </p>
-        
 
-        <ol className={`${styles.paragraph} list-decimal list-inside`}>
-            <li>Coordination and planning for production safe storage household’s including institutions food requirements and for various markets</li>
-            <li>Provide facilities and space for produce aggregation, loading and unloading ramps, value addition, drying and storage</li>
-            <li>Provide a platform for all service providers/ occupations especially youth and women to offer quality services to value chain actors at competitive rates</li>
-            <li>Provide training facilities including ITK knowledge exchange platform, nutrition, wellness and recreational center</li>
-        </ol>
+        {/* Points */}
+        <section id="features" className={layout.section}>
+          <div className={`${layout.sectionImg} flex-wrap items-center`}>
+            {points.map((feature, index) => (
+              <ListPoints key={feature.id} {...feature} index={index} />
+            ))}
+          </div>
+        </section>
         
     </div>
 
--- src/pages/TechnicalCommittee.jsx
+++ src/pages/TechnicalCommittee.jsx
@@ -1,5 +1,58 @@
 import { Helmet } from 'react-helmet-async';
-import styles from "../style";
+import styles, { layout } from "../style";
+import { star  } from "../assets";
+
+const points = [
+  {
+    id: "feature-1",
+    icon: star,
+    content:
+      "Capacity building of the committees.",
+  },
+  {
+    id: "feature-2",
+    icon: star,
+    content:
+      "Backstopping of planned activities.",
+  },
+  {
+    id: "feature-3",
+    icon: star,
+    content:
+      "Approval of the annual work plans and budgets.",
+  },
+  {
+    id: "feature-4",
+    icon: star,
+    content:
+      "Link partners to the committees for implementation of relevant projects.",
+  },
+  {
+    id: "feature-5",
+    icon: star,
+    content:
+      "Organize technology dissemination forums for the service providers, occupations and lead farmers.",
+  },
+  {
+    id: "feature-6",
+    icon: star,
+    content:
+      "Monitor all planned activities prepare and share independent performance reports.",
+  },
+];
+
+const ListPoints = ({ icon, content, index }) => (
+  <div className={`flex flex-col p-6 rounded-[20px] w-1/2 ${index !== points.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </div>
+);
 
 const TechnicalCommittee = () => (
   <div>
@@ -7,7 +60,7 @@ const TechnicalCommittee = () => (
         <title>Sub County and Ward Sustainable Resilient Food Systems Technical Committee</title>
     </Helmet>
     
-    {/* About */}
+    {/* About className="container w-full md:max-w-3xl mx-auto pt-20" */}
     <div className="container w-full md:max-w-3xl mx-auto pt-20">
         <h1 className={styles.heading2}>
         Sub County and Ward Sustainable Resilient Food Systems Technical Committee
@@ -27,16 +80,17 @@ const TechnicalCommittee = () => (
         <p className={`${styles.paragraph} mt-5`}>
         Finally, we emphasize the need to work together from the planning stages all through to implementation thus the need to form a standing committee with the following roles:
         </p>
-
-        <ol className={`${styles.paragraph} list-decimal list-inside`}>
-            <li>Capacity building of the committees</li>
-            <li>Backstopping of planned activities</li>
-            <li>Approval of the annual work plans and budgets</li>
-            <li>Link partners to the committees for implementation of relevant projects</li>
-            <li>Organize technology dissemination forums for the service providers, occupations and lead farmers</li>
-            <li>Monitor all planned activities prepare and share independent performance reports</li>
-        </ol>
         
+        {/* Points */}
+        <section id="features" className={layout.section}>
+          <div className={`${layout.sectionImg} flex-wrap items-center`}>
+            {points.map((feature, index) => (
+              <ListPoints key={feature.id} {...feature} index={index} />
+            ))}
+          </div>
+        </section>
+
+
     </div>
 
   </div>
